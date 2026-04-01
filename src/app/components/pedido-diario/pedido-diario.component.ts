a2895c79f766cfb4ba59614c04f4c93e2edb9c33
Fredy Hurtado <inztockz@gmail.com>
2026-03-31 19:38:34 -0500

    Actualizando el navbar y app main

---
+++ src/app/components/pedido-diario/pedido-diario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pedido-diario',
+  imports: [],
+  templateUrl: './pedido-diario.component.html',
+  styleUrl: './pedido-diario.component.css',
+})
+export class PedidoDiarioComponent {
+
+}
