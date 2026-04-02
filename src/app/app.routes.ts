437810b4e9bbb55b5a36009087948cb46d3216c5
Fredy Hurtado <inztockz@gmail.com>
2026-04-02 11:18:01 -0500

    Generando la vista de pedido diario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PedidoDiarioComponent } from './components/pedido-diario/pedido-diario.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "login", component: LoginComponent},
+    {path: "", component: NavbarComponent, 
+        children:[
+            {path: "", component:HomeComponent},
+            {path: "pedido-diario", component: PedidoDiarioComponent}
+        ]
+    }
+];
